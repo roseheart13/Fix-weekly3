8e5252ccf03d2c996f24e45d11bfaff4b9b0c40a
roseheart13 <70570030+roseheart13@users.noreply.github.com>
2020-09-22 12:39:15 +0200

    Some notes

--- Fix weekly3/Fix weekly3.cpp
+++ Fix weekly3/Fix weekly3.cpp
@@ -3,7 +3,9 @@
 #include <cstdlib>
 #include <time.h>
 
-int guessNumberA = 4;
+//randome = rand() % (så setter du inn de nummer du vil ha) 5 + 1;
+
+int guessNumberA;
 int guessNumberB = 16;
 int guessNumberC = 20;
 int number;
@@ -11,6 +13,8 @@ int count = 0;
 char option = 0;
 
 int main() {
+	// do wile loop for å få den til å forsete spillet flere ganger
+	
 	std::cout << "Welcome to guess the number!" << std::endl;
 	std::cout << " " << std::endl;
 	std::cout << "A = easy (1-5)" << std::endl;
@@ -41,22 +45,25 @@ int main() {
 }
 
 int easy() {
-	
-	std::cout << "Easy guess the number between 1-5" << std::endl;
-	while (number != guessNumberA) {
-		std::cout << "Guess a number" << std::endl;
-		std::cin >> number;
+	do {
+		std::cout << "Easy guess the number between 1-5" << std::endl;
+		while (number != guessNumberA) {
+			std::cout << "Guess a number" << std::endl;
+			std::cin >> number;
 
-		if (number == guessNumberA) {
-			std::cout << "Correct!" << std::endl;
-		}
-		else {
-			std::cout << "Wrong! Try again!" << std::endl;
-			std::cout << " " << std::endl;
-			count++;
-		}
-		std::cout << "It took you " << count << " times to guess the number" << std::endl;
+			// if (number < guessNumberA && number < 6)
 
+			if (number == guessNumberA) {
+				std::cout << "Correct!" << std::endl;
+			}
+			else {
+				std::cout << "Wrong! Try again!" << std::endl;
+				std::cout << " " << std::endl;
+				count++;
+			}
+			std::cout << "It took you " << count << " times to guess the number" << std::endl;
+
+		}
 	}
 }
 
